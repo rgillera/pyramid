7798c97a5f61b359d6bb4077a2526b4a397e9ae0
Rodel Gillera <rodelgillera@gmail.com>
2020-10-18 04:13:45 +0800

    Initial Setup.

---
+++ src/Providers/PyramidServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rgillera\Pyramid\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PyramidServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
