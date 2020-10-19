e6a9a2b28983c0fdb4176b7ca8aab6bace7a604a
Rodel Gillera <rodelgillera@gmail.com>
2020-10-20 05:20:36 +0800

    For cached routes

--- src/Providers/PyramidServiceProvider.php
+++ src/Providers/PyramidServiceProvider.php
@@ -23,6 +23,6 @@ class PyramidServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        include __DIR__.'/../routes.php';
+        $this->loadRoutesFrom(__DIR__.'/../routes.php');
     }
 }
